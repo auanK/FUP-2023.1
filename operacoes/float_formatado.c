2c8537e667d338d340dc2f24d4c489a808d1d35d
auanK <kauanpablo089@gmail.com>
2023-05-02 09:05:27 -0300

    refactor: alter float_formatado.c

---
+++ operacoes/float_formatado.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    int a, b;
+    float divi;
+
+    scanf("%d", &a);
+    scanf("%d", &b);
+
+    divi = (float)a / b;
+
+    printf("%d\n", a + b);
+    printf("%d\n", a - b);
+    printf("%d\n", a * b);
+    printf("%.2f\n", divi);
+    printf("%d\n", a % b);
+}
