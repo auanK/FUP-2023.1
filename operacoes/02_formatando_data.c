27543ebb9323beda075457e528a8049068614fe7
auanK <kauanpablo089@gmail.com>
2023-05-02 09:19:29 -0300

    refactor: alter 01_float_formatado.c

---
+++ operacoes/02_formatando_data.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    int hora, minuto, dia, mes, ano;
+
+    scanf("%d %d %d %d %d", &hora, &minuto, &dia, &mes, &ano);
+
+    hora = hora % 12;
+    printf("%02d:%02d %02d/%02d/%02d\n", hora, minuto, dia, mes, ano % 100);
+}
